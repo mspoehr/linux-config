9dc399f3faa7147df8fd2223536a68d6aab29eb2
Michael Spoehr <spoehrmichael@gmail.com>
2022-12-23 11:09:01 -0500

    Add -d option to file_install.sh to output diff against installed files

--- file-install.sh
+++ file-install.sh
@@ -7,27 +7,46 @@ source helper_scripts/local-helpers.sh
 
 # Plain files
 FILES_DIR=system_files
+DIFF_MODE=false
 
-install -m 644 $FILES_DIR/bashrc.sh ~/.bashrc
-install -m 644 $FILES_DIR/bash_profile.sh ~/.bash_profile
-install -m 644 $FILES_DIR/bash_aliases.sh ~/.bash_aliases
-install -m 644 $FILES_DIR/bash_functions.sh ~/.bash_functions
-install -m 644 $FILES_DIR/bash_prompt.sh ~/.bash_prompt
-install -m 644 $FILES_DIR/fzf.bash ~/.fzf.bash
-! $MAC || install -m 644 $FILES_DIR/brew_profile.sh ~/.brew_profile
+while getopts 'd' OPTION; do
+    case "$OPTION" in
+        d)
+            DIFF_MODE=true
+            ;;
+        ?)
+            echo "script usage: $0 [-d]" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if $DIFF_MODE; then
+    cmd="diff --unified=0"
+else
+    cmd="install -m 644"
+fi
+
+$cmd $FILES_DIR/bashrc.sh ~/.bashrc
+$cmd $FILES_DIR/bash_profile.sh ~/.bash_profile
+$cmd $FILES_DIR/bash_aliases.sh ~/.bash_aliases
+$cmd $FILES_DIR/bash_functions.sh ~/.bash_functions
+$cmd $FILES_DIR/bash_prompt.sh ~/.bash_prompt
+$cmd $FILES_DIR/fzf.bash ~/.fzf.bash
+! $MAC || $cmd $FILES_DIR/brew_profile.sh ~/.brew_profile
 mkdir -p ~/.config
-install -m 644 $FILES_DIR/starship.toml ~/.config/starship.toml
+$cmd $FILES_DIR/starship.toml ~/.config/starship.toml
 ! $MAC || touch ~/.hushlogin
-install -m 644 $FILES_DIR/dircolors.sh ~/.dircolors
-install -m 644 $FILES_DIR/vimrc ~/.vimrc
+$cmd $FILES_DIR/dircolors.sh ~/.dircolors
+$cmd $FILES_DIR/vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
-install -m 644 $FILES_DIR/badwolf.vim ~/.vim/colors/badwolf.vim
-install -m 644 $FILES_DIR/gitconfig ~/.gitconfig
-install -m 644 $FILES_DIR/gitignore_global ~/.gitignore_global
-install -m 644 $FILES_DIR/ripgreprc ~/.ripgreprc
-install -m 644 $FILES_DIR/com.googlecode.iterm2.plist ~/.config/com.googlecode.iterm2.plist
+$cmd $FILES_DIR/badwolf.vim ~/.vim/colors/badwolf.vim
+$cmd $FILES_DIR/gitconfig ~/.gitconfig
+$cmd $FILES_DIR/gitignore_global ~/.gitignore_global
+$cmd $FILES_DIR/ripgreprc ~/.ripgreprc
+$cmd $FILES_DIR/com.googlecode.iterm2.plist ~/.config/com.googlecode.iterm2.plist
 mkdir -p ~/Library/KeyBindings
-install -m 644 $FILES_DIR/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+$cmd $FILES_DIR/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
 
 # Configure git email, as this may vary per system:
 if [ -s ~/.gitemail ]; then
