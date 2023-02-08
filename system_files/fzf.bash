2025ef2ac98548ea4aeddea110f82e0a93ed5ee0
Michael Spoehr <spoehrmichael@gmail.com>
2023-02-08 13:22:13 -0500

    Rename fzf.bash variable

--- system_files/fzf.bash
+++ system_files/fzf.bash
@@ -5,14 +5,14 @@
 # fi
 
 if $MAC; then
-  brew_install_loc="$(brew --prefix)/opt/fzf/shell"
+  fzf_scripts_location="$(brew --prefix)/opt/fzf/shell"
 else
-  brew_install_loc="/usr/share/doc/fzf/examples"
+  fzf_scripts_location="/usr/share/doc/fzf/examples"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$brew_install_loc/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$fzf_scripts_location/completion.bash" 2> /dev/null
 _fzf_setup_completion path nano
 _fzf_setup_completion path vim
 # Note: host completion option not supported in lower version of fzf in linux package managers
@@ -22,4 +22,4 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Key bindings
 # ------------
-source "$brew_install_loc/key-bindings.bash"
+source "$fzf_scripts_location/key-bindings.bash"
