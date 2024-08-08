40b9a32bb8b097e403d65844fc5b15ff3ed770ab
Michael Spoehr <spoehrmichael@gmail.com>
2024-08-08 15:56:22 -0400

    Add t and gpushnow aliases

--- system_files/bash_aliases.sh
+++ system_files/bash_aliases.sh
@@ -9,7 +9,9 @@ alias ls='eza -F'
 alias ll='eza --git -alhF'
 alias l='eza -F'
 alias cat='bat --plain'
+alias t="terraform"
 alias g='git'
+alias gpushnow="git commit --amend --no-edit -a && git push --force-with-lease"
 alias awsp='awsprofile $(pick_aws_config)'
 
 which fdfind &>/dev/null && alias fd='fdfind'
