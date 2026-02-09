20d86e41503cd4e74e4b19ddad1d6882d647b1ba
Michael Spoehr <mspoehr@amazon.com>
2026-02-09 14:08:13 -0500

    Add ability for file-install.sh to install a particular file

--- file-install.sh
+++ file-install.sh
@@ -8,6 +8,7 @@ source helper_scripts/local-helpers.sh
 # Plain files
 FILES_DIR=system_files
 DIFF_MODE=false
+SPECIFIC_FILES=()
 
 while getopts 'd' OPTION; do
     case "$OPTION" in
@@ -15,10 +16,16 @@ while getopts 'd' OPTION; do
             DIFF_MODE=true
             ;;
         ?)
-            echo "script usage: $0 [-d]" >&2
+            echo "script usage: $0 [-d] [file1 file2 ...]" >&2
             exit 1
             ;;
     esac
+done
+shift $((OPTIND-1))
+
+# Collect specific files from remaining arguments
+for arg in "$@"; do
+    SPECIFIC_FILES+=("${arg#.}")
 done
 
 if $DIFF_MODE; then
@@ -27,41 +34,88 @@ else
     cmd="install -m 644"
 fi
 
-$cmd $FILES_DIR/bashrc.sh ~/.bashrc
-$cmd $FILES_DIR/bash_profile.sh ~/.bash_profile
-$cmd $FILES_DIR/bash_aliases.sh ~/.bash_aliases
-$cmd $FILES_DIR/bash_functions.sh ~/.bash_functions
-$cmd $FILES_DIR/bash_prompt.sh ~/.bash_prompt
-$cmd $FILES_DIR/fzf.bash ~/.fzf.bash
-! $MAC || $cmd $FILES_DIR/brew_profile.sh ~/.brew_profile
-mkdir -p ~/.config
-$cmd $FILES_DIR/starship.toml ~/.config/starship.toml
-! $MAC || touch ~/.hushlogin
-$cmd $FILES_DIR/dircolors.sh ~/.dircolors
-$cmd $FILES_DIR/vimrc ~/.vimrc
-mkdir -p ~/.vim/colors
-$cmd $FILES_DIR/badwolf.vim ~/.vim/colors/badwolf.vim
-$cmd $FILES_DIR/gitconfig ~/.gitconfig
-$cmd $FILES_DIR/gitignore_global ~/.gitignore_global
-$cmd $FILES_DIR/ripgreprc ~/.ripgreprc
+# File mapping: destination -> source
+declare -A FILE_MAP=(
+    [~/.bashrc]="$FILES_DIR/bashrc.sh"
+    [~/.bash_profile]="$FILES_DIR/bash_profile.sh"
+    [~/.bash_aliases]="$FILES_DIR/bash_aliases.sh"
+    [~/.bash_functions]="$FILES_DIR/bash_functions.sh"
+    [~/.bash_prompt]="$FILES_DIR/bash_prompt.sh"
+    [~/.fzf.bash]="$FILES_DIR/fzf.bash"
+    [~/.brew_profile]="$FILES_DIR/brew_profile.sh"
+    [~/.config/starship.toml]="$FILES_DIR/starship.toml"
+    [~/.dircolors]="$FILES_DIR/dircolors.sh"
+    [~/.vimrc]="$FILES_DIR/vimrc"
+    [~/.vim/colors/badwolf.vim]="$FILES_DIR/badwolf.vim"
+    [~/.gitconfig]="$FILES_DIR/gitconfig"
+    [~/.gitignore_global]="$FILES_DIR/gitignore_global"
+    [~/.ripgreprc]="$FILES_DIR/ripgreprc"
+    [~/.amethyst.yml]="$FILES_DIR/amethyst.yml"
+    [~/Library/KeyBindings/DefaultKeyBinding.dict]="$FILES_DIR/DefaultKeyBinding.dict"
+)
 
-if $MAC; then
-    $cmd $FILES_DIR/amethyst.yml ~/.amethyst.yml
+function should_install() {
+    local dest="$1"
     
-    mkdir -p ~/Library/KeyBindings
-    $cmd $FILES_DIR/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
-    for file in $FILES_DIR/preferences/*; do
-        $cmd "$file" ~/Library/Preferences/$(basename "$file")
+    # If no specific files requested, install all
+    if [ ${#SPECIFIC_FILES[@]} -eq 0 ]; then
+        return 0
+    fi
+    
+    # Check if destination matches any requested file
+    for file in "${SPECIFIC_FILES[@]}"; do
+        if [[ "$dest" == *"$file" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+function install_file() {
+    local dest="$1"
+    local src="$2"
+    local mac_only="${3:-false}"
+    
+    if ! should_install "$dest"; then
+        return
+    fi
+    
+    if $mac_only && ! $MAC; then
+        return
+    fi
+    
+    mkdir -p "$(dirname "$dest")"
+    $cmd "$src" "$dest"
+}
+
+# Install files
+for dest in "${!FILE_MAP[@]}"; do
+    src="${FILE_MAP[$dest]}"
+    mac_only=false
+    [[ "$dest" == *"brew_profile"* ]] || [[ "$dest" == *"amethyst"* ]] || [[ "$dest" == *"Library"* ]] && mac_only=true
+    install_file "$dest" "$src" "$mac_only"
+done
+
+# Special case: .hushlogin
+if $MAC && should_install "$HOME/.hushlogin" && [ ${#SPECIFIC_FILES[@]} -eq 0 ]; then
+    touch ~/.hushlogin
+fi
+
+# Special case: preferences directory
+if $MAC && ([ ${#SPECIFIC_FILES[@]} -eq 0 ] || should_install "preferences"); then
+    for file in "$FILES_DIR"/preferences/*; do
+        [ -f "$file" ] && install_file "$HOME/Library/Preferences/$(basename "$file")" "$file" true
     done
 fi
 
-# Configure git email, as this may vary per system:
-if [ -s ~/.gitemail ]; then
-    gitemail=$(cat ~/.gitemail)
-else
-    echo -n "Enter the default email for git commits on this system: "
-    read gitemail
-    echo "$gitemail" > ~/.gitemail
+# Configure git email only if installing gitconfig or no specific files
+if should_install "$HOME/.gitconfig"; then
+    if [ -s ~/.gitemail ]; then
+        gitemail=$(cat ~/.gitemail)
+    else
+        echo -n "Enter the default email for git commits on this system: "
+        read -r gitemail
+        echo "$gitemail" > ~/.gitemail
+    fi
+    git config --global user.email "$gitemail"
 fi
-
-git config --global user.email "$gitemail"
